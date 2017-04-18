f3144880c06b7ebe329a0f544cd8b29e6eaa9acd
yuanyanjun <yuanyanjun@baidu.com>
2017-04-18 15:34:52 +0800

    nest reducers webpack.config

--- redux/nest-reducers/webpack.config.js
+++ redux/nest-reducers/webpack.config.js
@@ -4,45 +4,28 @@
  */
 
 var path = require('path');
-var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function () {
-    return {
-        devtool: 'eval',
-        entry: [
-            'webpack-dev-server/client?http://localhost:3000',
-            'webpack/hot/only-dev-server',
-            './index'
-        ],
-        output: {
-            path: path.join(__dirname, 'dist'),
-            filename: 'bundle.js',
-            publicPath: '/static/'
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin()
-        ],
-        // resolve: {
-        //     alias: {
-        //         redux: path.join(__dirname, 'node_modules/redux')
-        //     },
-        //     extensions: ['', '.js']
-        // },
-        module: {
-            loaders: [{
-                test: /\.js$/,
-                loaders: ['react-hot', 'babel'],
-                exclude: /node_modules/,
-                include: __dirname
-            }, {
-                test: /\.js$/,
-                loaders: ['babel'],
-                include: path.join(__dirname, '..', '..', 'src')
-            }, {
-                test: /\.css?$/,
-                loaders: ['style', 'raw'],
-                include: __dirname
-            }]
-        }
-    };
+const BUILD_PATH = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    devServer: {
+        contentBase: BUILD_PATH
+    }
 };
